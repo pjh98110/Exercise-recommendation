8b56c3a118f764e74248aac23cdfcbf323a11d4e
Jonghyeon <128576949+pjh98110@users.noreply.github.com>
2023-11-29 22:43:19 +0900

    Update XAI&Profiling.py

--- pages/XAI&Profiling.py
+++ pages/XAI&Profiling.py
@@ -5,8 +5,8 @@ import numpy as np
 import os
 import datetime as dt
 from streamlit_extras.switch_page_button import switch_page
-import streamlit.components.v1 as components 
-import sweetviz as sv
+# import streamlit.components.v1 as components 
+# import sweetviz as sv
 
 
 if "page" not in st.session_state:
@@ -50,13 +50,13 @@ if selected_xai == "XAI_분류":
 
 
 # Sweetviz 프로파일링 실행
-elif selected_xai == "Sweetviz_Profiling":
-    if st.button("Sweetviz_Profiling 실행"):
-        # Sweetviz 보고서 생성
-        report = sv.analyze(data)
-        # HTML 파일로 보고서 저장
-        report.show_html(filepath='report.html', open_browser=False)
-        # 보고서 표시
-        HtmlFile = open('report.html', 'r', encoding='utf-8')
-        source_code = HtmlFile.read() 
-        components.html(source_code, height=800)
+# elif selected_xai == "Sweetviz_Profiling":
+#     if st.button("Sweetviz_Profiling 실행"):
+#         # Sweetviz 보고서 생성
+#         report = sv.analyze(data)
+#         # HTML 파일로 보고서 저장
+#         report.show_html(filepath='report.html', open_browser=False)
+#         # 보고서 표시
+#         HtmlFile = open('report.html', 'r', encoding='utf-8')
+#         source_code = HtmlFile.read() 
+#         components.html(source_code, height=800)
