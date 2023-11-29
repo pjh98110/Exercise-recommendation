aec12591a106868c9c0d58eb265e99c1e8d03365
Jonghyeon <128576949+pjh98110@users.noreply.github.com>
2023-11-29 22:58:21 +0900

    Update XAI&Profiling.py

--- pages/XAI&Profiling.py
+++ pages/XAI&Profiling.py
@@ -27,23 +27,23 @@ st.markdown(f"""
             """, unsafe_allow_html=True)
 
 
-selected_xai = st.selectbox(
-    label="원하는 XAI_분석을 선택하세요.",
-    options=["XAI_분류", "Sweetviz_Profiling"],
-    placeholder="하나를 선택하세요.",
-    help="선택한 XAI에 따라 다른 분석 결과를 제공합니다.",
-    key="xai_key")
+# selected_xai = st.selectbox(
+#     label="원하는 XAI_분석을 선택하세요.",
+#     options=["XAI_분류", "Sweetviz_Profiling"],
+#     placeholder="하나를 선택하세요.",
+#     help="선택한 XAI에 따라 다른 분석 결과를 제공합니다.",
+#     key="xai_key")
 
 
 
-if selected_xai == "XAI_분류":
-    if st.button("XAI 분류 실행"):
-        st.components.v1.html(
-            """
-            <iframe src="http://20.214.137.21:7777/" style="width:100%; height:600px;"></iframe>
-            """,
-            height=600,
-        )
+# if selected_xai == "XAI_분류":
+#     if st.button("XAI 분류 실행"):
+st.components.v1.html(
+    """
+    <iframe src="http://20.214.137.21:7777/" style="width:100%; height:600px;"></iframe>
+    """,
+    height=600,
+)
         
 
 # Sweetviz 프로파일링
